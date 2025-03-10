133303d3d82f21ec8432f030a11e3a0bc1796693
Synth Morxemplum <morxemplum@gmail.com>
2025-03-09 18:51:40 -0700

    Use scour to aggressively optimize final SVGs

---
+++ optimize_plain_svg.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=`pwd`
+CURSOR_DIR="${CURRENT_DIR}/hyprcursor_themes/mono_black/hyprcursors/"
+
+if [ ! -d "$CURSOR_DIR" ]; then
+    echo "ERROR: Cursor Directory does not exist."
+    exit 1
+fi
+
+for svg in `find $CURSOR_DIR -iname "*.svg"`; do
+    FILE_NAME="${svg%.*}"
+    scour $svg $FILE_NAME-optimized.svg --set-precision=4 --strip-xml-prolog \
+    --remove-titles --remove-description --remove-metadata --remove-descriptive-elements \
+    --enable-comment-stripping --indent=tab --no-line-breaks --strip-xml-space \
+    --enable-id-stripping --shorten-ids
+    rm $svg
+    mv $FILE_NAME-optimized.svg $svg
+done
