9355a4671b3f2dbd820ac8daa9538cedb3500dcd
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-04-02 23:46:32 +0100

    feat(build): remove interactive portion of buildVariant.sh now accepting it's first argument as the THEME value

--- plasma_themes/src/build_tools/buildVariant.sh
+++ plasma_themes/src/build_tools/buildVariant.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-BIN_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+BIN_DIR="$(dirname "${BASH_SOURCE[0]}")"
 THEMES_DIR="../.."
 SRC_DIR="$THEMES_DIR/src"
-THEME=""
+THEME=$1
 THEMES=("posys_cursor_scalable" "posys_cursor_scalable_black" "posys_cursor_scalable_mono" "posys_cursor_scalable_mono_black")
 VARIANTS=("white" "black" "mono" "mono_black")
 ALIASES="$SRC_DIR/alias.list"
@@ -17,34 +17,23 @@ TAIL_CURSORS=("alias" "context-menu" "copy" "help" "no-drop" "progress")
 TAIL_ICON_SIZE=32
 SCALES="75 100 125 150 175"
 
-## START OF SCRIPT
-echo "Select a cursor theme to generate rasters and aliases:"
-echo ""
-echo "1. Default (White)"
-echo "2. Black"
-echo "3. Mono"
-echo "4. Mono Black"
-echo ""
-echo -n "Answer (1 2 3 4): "
-read THEME
-THEME=$(expr $THEME - 1)
-
 VAR_DIR="$THEMES_DIR/${THEMES[$THEME]}"
 CURSOR_DIR="$VAR_DIR/cursors_scalable"
 VARIANT="${VARIANTS[$THEME]}"
 
+## START OF SCRIPT
 echo -ne "Checking Requirements...\\r"
 if [[ ! -d "${CURSOR_DIR}" ]]; then
 	echo -e "\\nFAIL: Missing \"${VARIANT}\" cursor theme"
 	exit 1
 fi
 
-if ! command -v inkscape > /dev/null ; then
+if ! command -v inkscape >/dev/null; then
 	echo -e "\\nFAIL: Inkscape not found. Make sure you install inkscape through your package manager"
 	exit 1
 fi
 
-if ! command -v xcursorgen > /dev/null ; then
+if ! command -v xcursorgen >/dev/null; then
 	echo -e "\\nFAIL: xcursorgen not found. Make sure you install xcursorgen through your package manager"
 	exit 1
 fi
@@ -56,10 +45,10 @@ for scale in $SCALES; do
 	mkdir -p "build_${VARIANT}/x$scale"
 done
 mkdir -p "build_${VARIANT}/config"
-echo -e "\033[0KMaking Folders... DONE";
+echo -e "\033[0KMaking Folders... DONE"
 
 echo "Generating pixmaps..."
-for SVG in `find $CURSOR_DIR -iname "*.svg"`; do
+for SVG in $(find $CURSOR_DIR -iname "*.svg"); do
 	BASENAME=${SVG##*/}
 	BASENAME=${BASENAME%.*}
 	genPixmaps="file-open:${SVG};"
@@ -77,11 +66,11 @@ for SVG in `find $CURSOR_DIR -iname "*.svg"`; do
 	for scale in $SCALES; do
 		DIR="build_${VARIANT}/x${scale}"
 		if [[ "${DIR}/${BASENAME}.png" -ot ${SVG} ]]; then
-			genPixmaps="${genPixmaps} export-width:$((${ACTUAL_SIZE}*scale/100)); export-height:$((${ACTUAL_SIZE}*scale/100)); export-filename:${DIR}/${BASENAME}.png; export-do;"
+			genPixmaps="${genPixmaps} export-width:$((${ACTUAL_SIZE} * scale / 100)); export-height:$((${ACTUAL_SIZE} * scale / 100)); export-filename:${DIR}/${BASENAME}.png; export-do;"
 		fi
 	done
 	if [ "$genPixmaps" != "file-open:${SVG};" ]; then
-		inkscape --shell < <(echo "${genPixmaps}") > /dev/null
+		inkscape --shell < <(echo "${genPixmaps}") >/dev/null
 	fi
 
 	echo "    $BASENAME... DONE"
@@ -99,7 +88,7 @@ $BIN_DIR/generate_cursors ${CURSOR_DIR} "build_${VARIANT}" ${OUTPUT} ${SCALES}
 echo "Generating cursor theme... DONE"
 
 echo -ne "Generating shortcuts...\\r"
-while read ALIAS ; do
+while read ALIAS; do
 	# Skip comments (#)
 	if [[ $ALIAS =~ ^#.* ]]; then
 		continue
@@ -119,9 +108,9 @@ while read ALIAS ; do
 	fi
 
 	ln -s "$FROM" "$OUTPUT/$TO"
-done < $ALIASES
+done <$ALIASES
 
-while read ALIAS ; do
+while read ALIAS; do
 	# Skip comments (#)
 	if [[ $ALIAS =~ ^#.* ]]; then
 		continue
@@ -139,7 +128,7 @@ while read ALIAS ; do
 	fi
 
 	ln -s "$FROM" "$CURSOR_DIR/$TO"
-done < $ALIASES
+done <$ALIASES
 echo -e "\033[0KGenerating shortcuts... DONE"
 
 echo "COMPLETE!"
